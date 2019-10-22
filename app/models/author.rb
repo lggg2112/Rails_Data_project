1b8ce20971ca82e790e48c43ece65fc8e9608a0d
lggg2112 <lggg2112@hanmail.net>
2019-10-22 00:10:47 -0500

    Add root to route

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,8 @@
 
 class Author < ApplicationRecord
   belongs_to :publisher
+  has_many :appointments
+  has_many :employees, through: :appointments
 
   validates :name, :address, :email, :phonenumber, :nationality, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'appointments/index'
   get 'books/index'
@@ -11,4 +13,6 @@ Rails.application.routes.draw do
   get 'employees/index'
   get 'employees/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'books#index'
 end
