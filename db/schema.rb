49e91928c0db8341385cd572d857f0f5d8a9d3b8
lggg2112 <lggg2112@hanmail.net>
2019-10-22 06:23:20 -0500

    Modifying Index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_22_043845) do
+ActiveRecord::Schema.define(version: 2019_10_22_104213) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "shcedule"
@@ -57,6 +57,14 @@ ActiveRecord::Schema.define(version: 2019_10_22_043845) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["publisher_id"], name: "index_employees_on_publisher_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "permalink"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "publishers", force: :cascade do |t|
