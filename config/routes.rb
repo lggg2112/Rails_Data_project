4ac6379c064a9774d94e363ba77f88e018cfdc48
lggg2112 <lggg2112@hanmail.net>
2019-10-21 23:52:23 -0500

    Add authors controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'authors/index'
+  get 'authors/show'
   get 'employees/index'
   get 'employees/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
